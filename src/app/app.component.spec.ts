0ff9619e90e63ce77104e90e5ab8a9162cc7bf51
bojidaryovchev <bojidaryovchevscience@gmail.com>
2021-06-10 16:44:14 +0300

    app.component removed test responsible for checking inside app.component.html since we deleted everything in there - now the tests pass

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ol-mcdonald-farm');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ol-mcdonald-farm app is running!');
-  });
 });
